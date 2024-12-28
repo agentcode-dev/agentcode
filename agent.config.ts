b51362a0c21377b1ea0bc483cb1a2be19b992b5e
agentnode-dev <agenthubdev@gmail.com>
2024-12-27 17:18:30 -0800

    Update agent.config.ts

--- agent.config.ts
+++ agent.config.ts
@@ -1,6 +1,6 @@
 export const agentConfig = {
   agentName: 'SupportAgent',
-  taskTypes: ['workflowAutomation', 'problemSolving', 'dataAnalysis'],
+  taskTypes: ['workflowAutomation', 'problemSolving', 'dataAnalysis', 'Rate-scan'],
   parallelProcessing: false,
   learningEnabled: false,
 
